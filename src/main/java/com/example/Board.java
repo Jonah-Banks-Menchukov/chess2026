13c7867eb8fe982269b3886ea34d5534767ba582
Jonah-Banks-Menchukov <jalejobanks@gmail.com>
2026-03-02 19:22:46 +0000

    efeds

--- src/main/java/com/example/Board.java
+++ src/main/java/com/example/Board.java
@@ -1,3 +1,5 @@
+//Jonah Banks
+//This class stores the board constructor and instance variables
 package com.example;
 
 import java.awt.Color;
@@ -23,18 +25,8 @@ import javax.swing.*;
 public class Board extends JPanel implements MouseListener, MouseMotionListener {
     // Resource location constants for piece images
     public static final String PICTURE_PATH = "/src/main/java/com/example/Pictures/";
-    private static final String RESOURCES_WBISHOP_PNG = PICTURE_PATH + "wbishop.png";
-    private static final String RESOURCES_BBISHOP_PNG = PICTURE_PATH + "bbishop.png";
-    private static final String RESOURCES_WKNIGHT_PNG = PICTURE_PATH + "wknight.png";
-    private static final String RESOURCES_BKNIGHT_PNG = PICTURE_PATH + "bknight.png";
-    private static final String RESOURCES_WROOK_PNG = PICTURE_PATH + "wrook.png";
-    private static final String RESOURCES_BROOK_PNG = PICTURE_PATH + "brook.png";
-    private static final String RESOURCES_WKING_PNG = PICTURE_PATH + "wking.png";
-    private static final String RESOURCES_BKING_PNG = PICTURE_PATH + "bking.png";
-    private static final String RESOURCES_BQUEEN_PNG = PICTURE_PATH + "bqueen.png";
-    private static final String RESOURCES_WQUEEN_PNG = PICTURE_PATH + "wqueen.png";
-    private static final String RESOURCES_WPAWN_PNG = PICTURE_PATH + "wpawn.png";
-    private static final String RESOURCES_BPAWN_PNG = PICTURE_PATH + "bpawn.png";
+    private static final String RESOURCES_WBISHOP_PNG = PICTURE_PATH+"wbishop.png";
+    private static final String RESOURCES_BBISHOP_PNG = PICTURE_PATH+"bbishop.png";
 
     //constant used to keep track of where the piece should be drawn when the user is dragging it
     private static final int PIECE_OFFSET = 24;
@@ -103,8 +95,10 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
     // it's up to you how you wish to arrange your pieces.
     void initializePieces() {
 
-        board[0][0].put(new Piece(true, RESOURCES_WKING_PNG));
-
+        board[0][2].put(new Piece(true,RESOURCES_WBISHOP_PNG));
+        board[0][5].put(new Piece(true, RESOURCES_WBISHOP_PNG));
+        board[7][2].put(new Piece(false,RESOURCES_BBISHOP_PNG));
+        board[7][5].put(new Piece(false,RESOURCES_BBISHOP_PNG));
     }
 
     public Square[][] getSquareArray() {
@@ -150,6 +144,10 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         if (sq.isOccupied() && sq.getOccupyingPiece().getColor() == whiteTurn) {
             currPiece = sq.getOccupyingPiece();
             fromMoveSquare = sq;
+            ArrayList<Square> l=currPiece.getControlledSquares(board, fromMoveSquare);
+            for(int i=0; i<l.size();i++){
+                l.get(i).setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.BLUE));
+            }
             sq.setDisplay(false);
         }
         repaint();
@@ -162,10 +160,22 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
     @Override
     public void mouseReleased(MouseEvent e) {
         endSquare = (Square) this.getComponentAt(new Point(e.getX(), e.getY()));
-
-        // using currPiece
+        for(Square[] row: board){
+            for(Square s:row){
+                s.setBorder(null);
+            }
+        }
         if(fromMoveSquare!= null){
-            fromMoveSquare.setDisplay(true);
+            if(currPiece!=null&&currPiece.getLegalMoves(this, fromMoveSquare).contains(endSquare)){
+                fromMoveSquare.setDisplay(false);
+                endSquare.put(currPiece);
+                endSquare.setDisplay(true);
+                fromMoveSquare.removePiece();
+                whiteTurn=!whiteTurn;
+            }else{
+                fromMoveSquare.setDisplay(true);
+                fromMoveSquare.put(currPiece);
+            }
         }
         currPiece = null;
         repaint();
--- src/main/java/com/example/Piece.java
+++ src/main/java/com/example/Piece.java
@@ -1,3 +1,5 @@
+//Jonah Banks
+//This stores the constructor for my piece, the jumping bishop
 package com.example;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -50,7 +52,40 @@ public class Piece {
     //return a list of every square that is "controlled" by this piece. A square is controlled
     //if the piece capture into it legally.
     public ArrayList<Square> getControlledSquares(Square[][] board, Square start) {
-     return null;
+    	ArrayList<Square> legals=new ArrayList<>();
+        int currRow=start.getRow();
+        int currCol=start.getCol();
+        //moving diagonally downwards
+        int col=currCol+1;
+        for(int row=currRow+1;row<8&&col<8; row++){
+            legals.add(board[row][col]);
+            col++;
+        }
+        col=currCol-1;
+        //moving diagonally upwards
+        for(int row2=currRow-1;row2>=0&&col>=0;row2--){
+            legals.add(board[row2][col]);
+            col--;
+        }
+        //moving down a row, moving up a column
+        col=currCol-1;
+        for(int row3=currRow+1;row3<8&&col>=0; row3++){
+            legals.add(board[row3][col]);
+            col--;
+        }
+        col=currCol+1;
+        //moving up a row, moving down a column
+        for(int row4=currRow-1; row4>=0&&col<8; row4--){
+            legals.add(board[row4][col]);
+            col++;
+        }
+        for(int i=0;i<legals.size();i++){
+            if(legals.get(i).isOccupied()&&legals.get(i).getOccupyingPiece().getColor()==color){
+                legals.remove(i);
+                i--;
+            }
+        }
+        return legals;
     }
     
 
@@ -60,35 +95,44 @@ public class Piece {
     //returns an arraylist of squares which are legal to move to
     //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
     //going to score any points.
+    //Precondition: called from a piece object; b and start are not null.
+    //Postcondition: returns an array con
     public ArrayList<Square> getLegalMoves(Board b, Square start){
     	ArrayList<Square> legals=new ArrayList<>();
         Square[][]  squares=b.getSquareArray();
         int currRow=start.getRow();
         int currCol=start.getCol();
         //moving diagonally downwards
-        int col=currCol;
-        for(int row=currRow;row<8&&col<8; row++){
+        int col=currCol+1;
+        for(int row=currRow+1;row<8&&col<8; row++){
             legals.add(squares[row][col]);
             col++;
         }
         col=currCol-1;
         //moving diagonally upwards
-        for(int row2=currRow-1;row2>0&&col>0;row2--){
+        for(int row2=currRow-1;row2>=0&&col>=0;row2--){
             legals.add(squares[row2][col]);
             col--;
         }
         //moving down a row, moving up a column
         col=currCol-1;
-        for(int row3=currRow+1;row3<8&&col>0; row3++){
+        for(int row3=currRow+1;row3<8&&col>=0; row3++){
             legals.add(squares[row3][col]);
             col--;
         }
         col=currCol+1;
         //moving up a row, moving down a column
-        for(int row4=currRow-1; row4>0&&col<8; row4--){
+        for(int row4=currRow-1; row4>=0&&col<8; row4--){
             legals.add(squares[row4][col]);
             col++;
         }
+        //Remove any squares where piece is same color
+        for(int i=0;i<legals.size();i++){
+            if(legals.get(i).isOccupied()&&legals.get(i).getOccupyingPiece().getColor()==color){
+                legals.remove(i);
+                i--;
+            }
+        }
+        return legals;
     }
-    return legals;
 }
